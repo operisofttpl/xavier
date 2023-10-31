dfeb893dba64020df76f6ce6bc4e2621e6601e61
operisofttpl <141032066+operisofttpl@users.noreply.github.com>
2023-10-31 12:40:14 +0530

    Updated file as per the requirements

--- variables.tf
+++ variables.tf
@@ -3,19 +3,9 @@ variable "instance_name1" {
   default  = "kali-rdp"
 }
 
-variable "instance_name2" {
-  type  = string
-  default  = "metasploit"
-}
-
 variable "instance_name3" {
   type  = string  
   default  = "Windows-10-Pro"
-}
-
-variable "instance_name4" {
-  type  = string
-  default  = "marlinspike"
 }
 
 variable "keypair_name" {
